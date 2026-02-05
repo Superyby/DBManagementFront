89895067d808f1719c084c4e09828acc71397c16
“Yu” <1551405455@qq.com>
2026-02-05 16:37:07 +0800

    管网JSX示例

--- src/App.tsx
+++ src/App.tsx
@@ -52,6 +52,17 @@ function App() {
           <p>API 地址: http://192.168.31.36:8080</p>
           <p>代理测试: http://192.168.31.36:3000/api/health</p>
         </div>
+        <h1>Hedy Lamarr's Todos</h1>
+        <img
+          src="https://i.imgur.com/yXOvdOSs.jpg"
+          alt="Hedy Lamarr"
+          className="photo"
+        />
+        <ul>
+          <li>Invent new traffic lights</li>
+          <li>Rehearse a movie scene</li>
+          <li>Improve the spectrum technology</li>
+        </ul>
       </header>
     </div>
   );
